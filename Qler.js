4ff9207e2cfd28c21e67bddd272ea5bd576a17b2
Chris Villa <chrisvilla@me.com>
2016-08-02 19:18:34 +0100

    Keyed blocks on concurrency

--- Qler.js
+++ Qler.js
@@ -1,31 +1,92 @@
 var Queue = function (concurrency) {
     var numProcessing = 0;
-    var fnQueue = [];
     var concurrency = concurrency || 1;
 
+    var lockedKeys = [];
+    var queueOfQueues = [];
+
+    var getNextQueueItem = function (index) {
+        var index = index || 0;
+        var chunk = queueOfQueues[index];
+
+        if (chunk === undefined) {
+            return undefined
+        } else if (chunk.length === 0) {
+            // Remove chunk from queueOfQueues as empty and start again
+            queueOfQueues.splice(index, 1);
+            return getNextQueueItem();
+        }
+
+        var queueItem = chunk[0];
+
+        if (lockedKeys.indexOf(queueItem.key) === -1) {
+            // Remove queueItem from chunk return it
+            chunk.splice(chunk.indexOf(queueItem), 1);
+
+            // Ensure we lock this key, unless default key
+            if (queueItem.key !== '') {
+                lockedKeys.push(queueItem.key)
+            }
+
+            return queueItem;
+        }  else {
+            return getNextQueueItem(index + 1);
+        }
+    }
+
     var processNext = function () {
-        if (numProcessing < concurrency && fnQueue.length) {
-            var nextCall = fnQueue.shift();
-            if (nextCall !== undefined) {
-                // Check again before assigning to be sure no race condition
-                if (numProcessing <= concurrency) {
-                    numProcessing++;
+        if (numProcessing < concurrency && queueOfQueues.length) {
+            var queueItem = getNextQueueItem();
 
-                    isProcessing = true;
+            if (queueItem !== undefined) {
 
-                    nextCall(function () {
-                        numProcessing--;
-                        processNext();
-                    });
+                var nextFn = queueItem.fn;
+                var nextKey = queueItem.key;
 
-                    return;
-                };
+                numProcessing++;
+
+                isProcessing = true;
+
+                nextFn(function () {
+                    numProcessing--;
+
+                    if (lockedKeys.indexOf(nextKey) >= 0) {
+                        lockedKeys.splice(lockedKeys.indexOf(nextKey));
+                    };
+
+                    processNext();
+                });
+
+                return;
             }
         };
     };
 
-    var queue = function (fn) {
-        fnQueue.push(fn);
+    var queue = function (fn, key) {
+        var key = key || '';
+
+        var queueItem = {
+            fn: fn,
+            key: key
+        }
+
+        var previousQueueChunk = queueOfQueues[queueOfQueues.length - 1];
+
+        if (previousQueueChunk !== undefined) {
+            var previousQueueItem = previousQueueChunk[0];
+            if (previousQueueItem !== undefined) {
+                if (previousQueueItem.key === queueItem.key) {
+                    queueOfQueues[queueOfQueues.length - 1].push(queueItem);
+                } else {
+                    queueOfQueues.push([queueItem]);
+                }
+            } else {
+                queueOfQueues.push([queueItem]);
+            }
+        } else {
+            queueOfQueues.push([queueItem]);
+        }
+
         processNext();
     };
 
--- README.md
+++ README.md
@@ -34,7 +34,7 @@ Qler also supports concurrency. Just specify the number of concurrent queues whe
     // Allow up to 5 concurrent operations
     var myQueue = Qler(5);
 
-Just wrap in another closure if you need to pass a variable.
+If you need to pass a variable, wrap in another closure.
 
     var x;
 
@@ -61,3 +61,34 @@ Just wrap in another closure if you need to pass a variable.
             }, 2000);
         };
     }(x));
+
+When using concurrency, you can make only certain functions concurrent. To do this, you must specify the second `key` parameter. A function with the `key` parameter will only execute after the last function with the same `key` parameter has finished, regardless of the `concurrency` setting. For example:
+
+    // Allow up to 5 concurrent operations
+    var myQueue = Qler(5);
+
+    myQueue.queue(function(callback) {
+        console.log('Waiting 2s');
+        setTimeout(function () {
+            console.log('The Rabbit is in the hole!');
+            callback()
+        }, 2000);
+    }, 'animal');  // Specify key here
+
+    // Won't execute until first `animal` method is finished
+    myQueue.queue(function(callback) {
+        console.log('Waiting another 2s');
+        setTimeout(function () {
+            console.log('The Badger is in the hole!');
+            callback()
+        }, 2000);
+    }, 'animal');  // Specify key here
+
+    // Will execute as normal
+    myQueue.queue(function(callback) {
+        console.log('Waiting another 2s');
+        setTimeout(function () {
+            console.log('The TV is in the hole!');
+            callback()
+        }, 2000);
+    }); // No key by default. This will use concurrency
